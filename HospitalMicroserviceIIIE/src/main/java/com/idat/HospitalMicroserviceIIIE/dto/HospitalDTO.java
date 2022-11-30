253f3ea11384b87b48413ef98b10f94f2aa45b8d
Josue Daniel Agurto Pulache <agurtodani@gmail.com>
2022-11-29 21:28:41 -0500

    dto

---
+++ HospitalMicroserviceIIIE/src/main/java/com/idat/HospitalMicroserviceIIIE/dto/HospitalDTO.java
@@ -0,0 +1,37 @@
+package com.idat.HospitalMicroserviceIIIE.dto;
+
+public class HospitalDTO {
+
+	private Integer cod;
+
+	private String nom;
+
+	private Double pre;
+
+	public Integer getCod() {
+		return cod;
+	}
+
+	public void setCod(Integer cod) {
+		this.cod = cod;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Double getPre() {
+		return pre;
+	}
+
+	public void setPre(Double pre) {
+		this.pre = pre;
+	}
+	
+	
+
+}
