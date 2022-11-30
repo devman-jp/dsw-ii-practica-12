547f7520907c1554426110a67403f9fbf17a6a70
Jesus Toledo <jesusplasencia2508@hotmail.com>
2022-11-29 20:32:53 -0500

    First Up Practice

---
+++ ClienteMicroserviceIIIE/src/main/java/com/idat/ClienteMicroserviceIIIE/ClienteMicroserviceIiieApplication.java
@@ -0,0 +1,13 @@
+package com.idat.ClienteMicroserviceIIIE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteMicroserviceIiieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteMicroserviceIiieApplication.class, args);
+	}
+
+}
---
+++ HospitalMicroserviceIIIE/src/test/java/com/idat/HospitalMicroserviceIIIE/HospitalMicroserviceIiieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.HospitalMicroserviceIIIE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalMicroserviceIiieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
